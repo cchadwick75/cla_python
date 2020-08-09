6b65f80b9dab03665542b892694e30b37bb0750b
collin <cchadwick75@gmail.com>
2020-08-08 21:31:32 -0400

    almost all functionality is working

--- pycommand.py
+++ pycommand.py
@@ -3,15 +3,20 @@ import sys
 import json
 import datetime
 
-
-def convert_time(time_var):
-    return datetime.datetime.fromtimestamp(int(time_var)).strftime('%Y-%m-%d %H:%M:%S')
 # argument = sys.argv[1:]
 # print(argument)
 
-argument = ['-pass', -18.9240, 96.6139]
-if argument == '-loc':
+def convert_time(time_var):
+    return datetime.datetime.fromtimestamp(int(time_var)).strftime('%Y-%m-%d %H:%M:%S')
 
+def get_names(people):
+    return ', '.join([x['name'] for x in people if 'name' in x])
+
+def get_craft(craft):
+    return  ', '.join(set([x['craft'] for x in craft if 'craft' in x]))
+
+argument = ['--people']
+if argument == '-loc':
     response = requests.get('http://api.open-notify.org/iss-now.json')
 
     if response.status_code == 200:
@@ -19,20 +24,30 @@ if argument == '-loc':
         print(f"The ISS current location at {convert_time(response_dict['timestamp'])} is "
               f"[{response_dict['iss_position']['latitude']}, {response_dict['iss_position']['longitude']}]")
 
-elif '-pass' in argument:
+elif '--pass' in argument:
     latitude = argument[1]
     longitude = argument[2]
 
     response = requests.get(f'http://api.open-notify.org/iss-pass.json?lat={latitude}&lon={longitude}')
     response_dict = json.loads(response.text)
-    for future_pass in response_dict['request']:
-        if
-    print(f"The ISS current location at {convert_time(response_dict['timestamp'])} is "
-          f"[{response_dict['iss_position']['latitude']}, {response_dict['iss_position']['longitude']}]")
+
+    if response_dict['message'] == 'success':
+        for x in response_dict['response']:
+            print(f"The ISS will be overhead {latitude}, {longitude} at {convert_time(x['risetime'])} for {x['duration']} seconds")
+    else:
+        print(f"error returned from ISS, see message ->:{response_dict['message']} ")
+
+elif '--people' in argument:
+
+    response = requests.get(f'http://api.open-notify.org/astros.json')
+    response_dict = json.loads(response.text)
+
+    if response_dict['message'] == 'success':
+        print(f"There are {response_dict['number']} aboard the {get_craft(response_dict['people'])}.  They are {get_names(response_dict['people'])}.")
+    else:
+        print(f"error returned from ISS, see message ->:{response_dict['message']} ")
 
 
-elif argument == '-people':
-    pass
 
 
 
